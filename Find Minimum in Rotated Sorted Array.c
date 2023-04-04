f0320561e7589b2aa0d4b502a8eed1cdd7b26692
Deyan Lafchiev <111605228+deyanlaf0409@users.noreply.github.com>
2023-04-04 16:08:50 +0300

    Add files via upload

---
+++ Find Minimum in Rotated Sorted Array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int search(int a[], int s, int e){
+    int m = s+(e-s)/2;
+
+    if(a[s] > a[e]){
+        if(a[m] > a[m+1]){
+            return a[m+1];
+        }
+    }
+    else{
+        return a[s];
+    }
+
+
+}
+
+int main(){
+
+    int a[] = {4,5,6,7,0,1,2};
+    int size = sizeof(a)/sizeof(a[0]);
+    int s = 0;
+    int e = size-1;
+    int res = search(a,s,e);
+    printf("%d",res);
+    }
